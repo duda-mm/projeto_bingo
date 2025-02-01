8db477aa84b496d86d0ab80c6228a1df4201b7e6
Duda M <dudamdemedeiros@gmail.com>
2025-01-31 21:51:17 -0300

    cartela

--- bingo.py
+++ bingo.py
@@ -1,1 +1,13 @@
-print('hello bingo')
+import random
+
+#gerar a cartela
+def gerar_cartela(jogador, linhas, colunas, intervalos):
+    cartela=[]
+    for _ in range(linhas):
+        linha = [random.choice(range(intervalos[i][0], intervalos[i][1]+1)) for i in range (colunas)]
+        cartela.append(linha)
+    return{
+        'jogador':jogador,
+        'numeros': cartela,
+        'num_marcados':set()
+    }
